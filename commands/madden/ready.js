1879b5acb5a5ba7173a617dd77daa83663b38ca5
the-random-encounter <31645891+the-random-encounter@users.noreply.github.com>
2024-12-03 04:58:00 -0600

    Corrected errors in code

--- commands/madden/ready.js
+++ commands/madden/ready.js
@@ -2,6 +2,7 @@ const { SlashCommandBuilder } = require('discord.js');
 const { memberId } = require('../../config.json');
 const { User, Season } = require('../../models/models.js');
 const { weekNumberFancy } = require('../../utils/functions.js');
+const Op = require('sequelize').Op;
 
 module.exports = {
   data: new SlashCommandBuilder()
@@ -11,35 +12,29 @@ module.exports = {
 
     const member = interaction.member;
 
-
-    if (!member.roles.cache.has(memberId)) {
+    if (!member.roles.cache.has(memberId))
       await interaction.reply({ content: `${interaction.user},\nyou must be a member of the League to signify week advancement readiness.`, ephemeral: true}); 
-    } else {
-      
-      await User.update( { weekFinished: true },
-        { where: { 
-          discordName: member.toString(),
-        },
-      });
+    else {
 
-      await User.save();
+      const userToUpdate = await User.findOne({ where: { discordName: member.displayName }});
 
-      const allFinished = await User.findOne( { where: { weekFinished: false }} );
+      await userToUpdate.update( { weekFinished: true } );
+
+      const unfinishedUser = await User.findOne( { where: { weekFinished: false }} );
 
       const test = await User.findOne( { where: { weekFinished: true }} );
 
       console.log(test);
 
-      if (allFinished === null ) {
+      if (unfinishedUser === null ) {
 
         const weekFancy = await weekNumberFancy();
-        await interaction.reply({ content: `@${interaction.member},\nYou have successfully flagged that you are finished for ${weekFancy}. All teams are now ready to advance.`, ephemeral: true });
+        await interaction.reply({ content: `${interaction.member},\nYou have successfully flagged that you are finished for ${weekFancy}. All teams are now ready to advance.`, ephemeral: true });
 
-        const announcementChannel = client.channels.cache.get('1307729794221342783');
-        const leagueMemberRole = channel.server.roles.get;
+        const announcementChannel = await interaction.guild.channels.cache.get('1307729794221342783');
 
-        announcementChannel.send(`@<1307730299840499732>, all teams have now finished their weekly requirements. The League will now soon advance.`);
-        announcementChannel.send({ content: `@<1307730011880685668>, please advance the week and notify me when it is complete.`, ephemeral: true });
+        announcementChannel.send(`<@&1307730299840499732>, all teams have now finished their weekly requirements. The League will now soon advance.`);
+        announcementChannel.send({ content: `<@&1307730011880685668>, please advance the week and notify me when it is complete.`, ephemeral: true });
 
       } else {
 
